7416eeea72743275161a7d18287702ecb91f3a4a
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-04-17 21:33:36 +0900

    add: deeplのauth_keyをconfig設定

--- api/app/Http/Controllers/DeepLController.php
+++ api/app/Http/Controllers/DeepLController.php
@@ -10,4 +10,14 @@ class DeepLController extends Controller
     {
         //
     }
+
+    public function connectDeepL(Request $request)
+    {
+        
+        $param = array(
+            'auth_key' => config('deepl_auth_key'), // 認証キーをここに設定
+            'text' => $request->get('transcript'),
+            'target_lang' => $request->get('lang')
+        );
+    }
 }
---
+++ api/config/deepl.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'deepl_auth_key' => env('DEEPL_AUTH_KEY')
+];
