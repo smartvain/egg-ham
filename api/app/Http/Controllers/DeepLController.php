1bd9b57a9dbf8e12df42d7ad3bb7e93edbee53f3
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-04-17 21:16:56 +0900

    add: DeepLController追加

---
+++ api/app/Http/Controllers/DeepLController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class DeepLController extends Controller
+{
+    public function __construct()
+    {
+        //
+    }
+}
