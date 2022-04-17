ccbe96e85806303412358645d84cc626cab04a84
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-04-17 20:54:49 +0900

    add: 言語リストを取得するメソッドを定義

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -2,6 +2,15 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <!-- test -->
+        <ValidationProvider v-slot="{ errors }" rules="required" name="URL">
+          <v-text-field
+            v-model="videoUrl"
+            :error-messages="errors"
+            label="URLを入力"
+            dense solo
+            @blur="langList = []; getLangList()"
+          />
+        </ValidationProvider>
     </v-col>
   </v-row>
 </template>
@@ -14,11 +23,27 @@ export default {
       {text: 'id', value: 'id'},
       {text: 'name', value: 'name'}
     ],
-    users: []
+    langList: [],
+    loading: {
+      getLangList: false,
+    },
+    videoUrl: ''
   }),
   methods: {
-    async getUsers() {
-      this.users = await this.$axios.$get('getUsers')
+    async getLangList() {
+      this.loading.getLangList = true
+
+      try {
+        const url = new URL(this.videoUrl)
+        this.langList = await this.$axios.$get('langList', {params: {
+          videoId: url.searchParams.get('v'),
+        }})
+      } catch(e) {
+        console.log('動画が存在しません。')
+      }
+
+      this.loading.getLangList = false
+    },
     }
   }
 }
