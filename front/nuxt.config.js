4b0fcdffde29391ff4b58b5740b93982e71103a3
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-04-24 19:49:10 +0900

    remove: エラーが出たのでローマ字変換ライブラリを削除

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -36,8 +36,7 @@ export default {
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
-    'js-hira-kata-romanize'
+    '@nuxtjs/vuetify'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
