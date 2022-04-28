bde123a223cc6f3e4a560fc0d660fa012230dcc9
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-04-29 00:06:09 +0900

    add: moment追加

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -33,18 +33,20 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
-    // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify'
+    '@nuxtjs/vuetify',
+    '@nuxtjs/moment'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     '@nuxtjs/toast'
   ],
+
+  moment: {
+    locales: ['ja']
+  },
 
   toast: {
     duration: 3000
