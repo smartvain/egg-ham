977c26eb6c4d90541b017fda3e3da81e62d0f071
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-05-08 15:25:29 +0900

    change: 見た目の隙間などを調整、字幕を絞り込みできるように変更

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <ValidationObserver v-slot="{ passes, validate }">
     <v-container fluid>
-      <v-row class="mt-4">
+      <v-row class="mt-0">
         <v-col cols="8">
           <v-row justify="center">
             <!-- URL入力欄 -->
@@ -32,12 +32,12 @@
             <!-- 言語選択欄 -->
             <v-col cols="3">
               <v-select
-                v-model="selectLang"
+                v-model="selectLang.caption"
                 :loading="loading.getLangList"
                 :items="langList"
                 item-text="text"
                 item-value="value"
-                placeholder="言語を選択"
+                placeholder="字幕言語を選択"
                 no-data-text="URLが入力されていません"
                 dense
               >
@@ -53,40 +53,56 @@
             </v-col>
           </v-row>
 
-          <v-row justify="center" class="mt-4">
-            <v-col cols="9" v-if="!caption">
-              <!-- サムネイル表示欄 -->
-              <v-img
-                :src="`https://img.youtube.com/vi/${videoInfo.id}/maxresdefault.jpg`"
-                :aspect-ratio="16/9"
-              />
-
-              <!-- 字幕取得ボタン -->
-              <v-row justify="center" class="mt-4">
-                <v-btn
-                  :disabled="!selectLang"
-                  color="primary"
-                  class="my-4"
-                  @click="validate().then(passes(getCaption))"
-                >
-                  字幕を取得する
-                </v-btn>
-              </v-row>
-            </v-col>
-
-            <v-col cols="10" v-else>
-              <!-- 字幕表示エリア -->
+          <v-row justify="center" class="mt-n6">
+            <v-col cols="10">
               <v-card
                 class="overflow-y-auto overflow-x-hidden"
-                height="500"
                 outlined
               >
-                <v-card-text>
-                  {{ caption }}
-                </v-card-text>
+                <v-data-table
+                  :headers="headers"
+                  :items="captions"
+                  :items-per-page="-1"
+                  :search="searchTime"
+                  height="550"
+                  fixed-header
+                  hide-default-footer
+                >
+                  <template #top>
+                    <v-text-field
+                      v-model="searchTime"
+                      placeholder="search"
+                      class="mx-2 mt-4"
+                      dense
+                    />
+                  </template>
+
+                  <!-- <template v-slot:[`item.start`]="{ item }">
+                    {{ item.start | calcTime }}
+                  </template> -->
+
+                  <template #no-data>
+                    <!-- サムネイル表示エリア -->
+                    <v-img
+                      :src="`https://img.youtube.com/vi/${videoInfo.id}/maxresdefault.jpg`"
+                      :aspect-ratio="16/9"
+                    />
+
+                    <!-- 字幕取得ボタン -->
+                    <v-row justify="center" class="mt-2">
+                      <v-btn
+                        :disabled="!selectLang.caption"
+                        color="primary"
+                        class="my-4"
+                        @click="validate().then(passes(getCaption))"
+                      >
+                        字幕を取得する
+                      </v-btn>
+                    </v-row>
+                  </template>
+                </v-data-table>
               </v-card>
             </v-col>
-            
           </v-row>
         </v-col>
 
@@ -94,22 +110,31 @@
 
         <v-col cols="4">
           <v-row justify="center">
-            <v-row justify="center">
-              <v-col cols="10">
-                <v-card
-                  class="overflow-y-auto overflow-x-hidden"
-                  height="500"
-                  outlined
-                >
-                  <v-card-text>
-                    {{ caption }}
-                  </v-card-text>
-                </v-card>
-              </v-col>
-            </v-row>
+            <v-col cols="10">
+              <v-select
+                v-model="selectLang.translate"
+                :items="translateLang"
+                item-text="text"
+                item-value="value"
+                placeholder="翻訳言語を選択"
+                no-data-text="翻訳言語がありません"
+                dense
+              />
+            </v-col>
           </v-row>
 
-          <v-row justify="center">
+          <v-row justify="center" class="mt-4">
+            <v-col cols="10">
+              <v-card
+                class="overflow-y-auto overflow-x-hidden"
+                height="300"
+                outlined
+              >
+              </v-card>
+            </v-col>
+          </v-row>
+
+          <!-- <v-row justify="center">
             <v-card color="#F6F9F9" width="90%" outlined>
               <v-list color="#F6F9F9">
                 <v-subheader class="font-weight-bold">今週のプレイ動画ランキング！</v-subheader>
@@ -134,9 +159,8 @@
                 </v-list-item-group>
               </v-list>
             </v-card>
-          </v-row>
+          </v-row> -->
         </v-col>
-        
       </v-row>
     </v-container>
   </ValidationObserver>
@@ -158,14 +182,25 @@ export default {
       bgColor: '#EEEEEE',// 薄いグレー
       isFocus: false
     },
-    caption: '',
+    translateLang: [
+      { text: '日本語', value: 'JA' }
+    ],
+    headers: [
+      { text: 'time', value: 'start', width: 75 },
+      { text: 'caption', value: 'caption' }
+    ],
+    captions: [],
     langList: [],
-    selectLang: null,
+    selectLang: {
+      caption: null,
+      translate: null
+    },
+    searchTime: null
   }),
   created() {
     this.videoInfo.url = 'https://www.youtube.com/watch?v=NoJXn-Fh6CU&t=19s'
     this.videoInfo.id = 'NoJXn-Fh6CU'
-    this.selectLang = 'en-US'
+    this.selectLang.caption = 'en-US'
 
     this.getCaption()
   },
@@ -190,19 +225,27 @@ export default {
       this.loading.getCaption = true
 
       try {
-        const res = await this.$axios.$get('caption', {params: {
+        var res = await this.$axios.$get('caption', {params: {
           videoId: this.videoInfo.id,
-          lang: this.selectLang
+          lang: this.selectLang.caption
         }})
-        this.caption = res.captions.join('')
-                                  .replace(/\r?\n/g, '')// 改行削除
-                                  .replace(/\s+/g, ' ')// 空行削除
-        console.log(this.caption)
       } catch(e) {
         this.$toast.error('字幕が存在しません。')
       }
 
+      for (const el of res) {
+        el.start = this.calcTime(el.start)
+      }
+      this.captions = res
+
       this.loading.getCaption = false
+    },
+    calcTime(time) {
+      const min = Math.floor(time % 3600 / 60);
+      let rem = String(Math.floor(time % 60));
+      if (rem < 10) { rem = rem.padStart(2, '0') }
+
+      return `${min}:${rem}`
     }
   }
 }
