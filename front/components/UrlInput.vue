3bfda6801998f5cec024e9fb343737e5e6cb4c51
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-05-14 15:29:27 +0900

    add: コンポーネントを追加

---
+++ front/components/UrlInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <ValidationProvider
+    v-slot="{ errors }"
+    rules="is_youtube_url"
+    name="URL"
+  >
+    <v-text-field
+      v-model="videoInfo.url"
+      :error-messages="errors"
+      :background-color="inputUrlArea.bgColor"
+      :flat="!inputUrlArea.isFocus"
+      placeholder="URLを入力"
+      dense solo clearable
+      @focus="
+        inputUrlArea.isFocus = true
+        inputUrlArea.bgColor = 'white'"
+      @blur="
+        inputUrlArea.isFocus = false
+        inputUrlArea.bgColor = '#EEEEEE'
+        if (videoInfo.url) { langList = []; getLangList() }"
+      @change="initCaption()"
+      @click:clear="initCaption()"
+    />
+  </ValidationProvider>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
