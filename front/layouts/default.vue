e04c266d904746c76263da5b44f0b3ab8c6dbb43
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-05-14 11:28:31 +0900

    add: ヘッダーにマイぺージ画面遷移ボタン追加

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -28,17 +28,23 @@
     <v-app-bar :clipped-left="clipped" fixed app elevation="2" height="50">
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
 
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
+      <!-- <v-btn icon @click.stop="miniVariant = !miniVariant">
         <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      </v-btn> -->
+
+      <v-btn
+        icon
+        @click.stop="clipped = !clipped"
+        :to="{
+          path: toggleUrl,
+        }"
+      >
+        <v-icon>mdi-cog-outline</v-icon>
       </v-btn>
 
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-
-      <v-btn icon @click.stop="fixed = !fixed">
+      <!-- <v-btn icon @click.stop="fixed = !fixed">
         <v-icon>mdi-minus</v-icon>
-      </v-btn>
+      </v-btn> -->
 
       <v-toolbar-title v-text="title" />
       
@@ -73,28 +79,37 @@
 <script>
 export default {
   name: 'DefaultLayout',
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
+  data: () => ({
+    clipped: false,
+    drawer: false,
+    fixed: false,
+    items: [
+      {
+        icon: 'mdi-apps',
+        title: 'Welcome',
+        to: '/',
+      },
+      {
+        icon: 'mdi-chart-bubble',
+        title: 'Inspire',
+        to: '/inspire',
+      },
+    ],
+    miniVariant: false,
+    right: true,
+    rightDrawer: false,
+    title: 'Vuetify.js',
+  }),
+  computed: {
+    toggleUrl() {
+      let path
+      switch (this.$route.path) {
+        case '/'      : path = '/mypage'; break
+        case '/mypage': path = '/'      ; break
+      }
+
+      return path
     }
-  },
+  }
 }
 </script>
