fd7b0610512879ea5fa4f2fdbaeefad69d5e3b5a
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-05-19 23:05:00 +0900

    refactor: DB処理をモデルの移動

--- api/app/Http/Controllers/WordController.php
+++ api/app/Http/Controllers/WordController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Word;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class WordController extends Controller
 {
+    private $word;
+
+    public function __construct(Word $word)
+    {
+        $this->word = $word;
+    }
+
     public function getWords()
     {
         return Word::get();
@@ -15,13 +21,6 @@ class WordController extends Controller
 
     public function storeWords(Request $request)
     {
-        $words = $request->all();
-        
-        DB::transaction(function () use ($words) {
-            foreach ($words as $word) {
-                $instance = new Word();
-                $instance->fill($word)->save();
-            }
-        });
+        $this->word->store($request->all());
     }
 }
--- api/app/Models/Word.php
+++ api/app/Models/Word.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Word extends Model
 {
@@ -14,4 +15,14 @@ class Word extends Model
         'mean',
         'word_type'
     ];
+
+    public function store($words)
+    {
+        DB::transaction(function () use ($words) {
+            foreach ($words as $word) {
+                $instance = new Word();
+                $instance->fill($word)->save();
+            }
+        });
+    }
 }
