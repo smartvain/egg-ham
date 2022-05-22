28c6939eb6172545825dc4a98b7ca30794dd2b26
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-05-23 01:14:04 +0900

    add: VarificationControllerを追加

---
+++ api/app/Http/Controllers/Auth/VerificationController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class VerificationController extends Controller
+{
+    public function verify($user_id, Request $request) {
+        if (!$request->hasValidSignature()) {
+            return response()->json(["msg" => "Invalid/Expired url provided."], 401);
+        }
+    
+        $user = User::findOrFail($user_id);
+    
+        if (!$user->hasVerifiedEmail()) {
+            $user->markEmailAsVerified();
+        }
+    
+        return redirect()->to('http://dev.yuleapp.com:1080/');
+    }
+    
+    // public function resend() {
+    //     if (auth()->user()->hasVerifiedEmail()) {
+    //         return response()->json(["msg" => "Email already verified."], 400);
+    //     }
+    
+    //     auth()->user()->sendEmailVerificationNotification();
+    
+    //     return response()->json(["msg" => "Email verification link sent on your email id"]);
+    // }
+}
