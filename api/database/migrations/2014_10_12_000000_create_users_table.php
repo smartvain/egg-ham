ab47e11cec78e453c75051dbc104273b56c53aa2
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-05-27 23:28:12 +0900

    change: twitter認証情報を格納するためにusersテーブルを修正

--- api/database/migrations/2014_10_12_000000_create_users_table.php
+++ api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,9 +18,13 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->string('provider_id')->nullable(); // required for twitter login
+            $table->string('provider_name')->nullable(); // required for twitter login
             $table->rememberToken();
             $table->timestamps();
+
+            $table->unique(['provider_id', 'provider_name']);
         });
     }
 
