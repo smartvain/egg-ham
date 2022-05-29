ef66a581f014af3621f08e16c0ed8337bab02b0b
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-05-29 19:47:00 +0900

    change: google認証ページを一新

--- front/pages/oauth/google/callback.vue
+++ front/pages/oauth/google/callback.vue
@@ -1,39 +1,54 @@
 <template>
+<v-container fill-height>
   <v-row justify="center" align-content="center">
-    <v-col cols="1" align="center">
-      認証中…
+    <v-col cols="12" align="center">
+      <v-card-text class="text-h5" style="color: #979797">
+        Google認証中…
+      </v-card-text>
+
+      <VueLoading
+        type="barsCylon"
+        color="#1095FE"
+        :size="{ width: '50px', height: '50px' }"
+      />
     </v-col>
   </v-row>
+</v-container>
 </template>
 
 <script>
-  export default {
-    async mounted() {
-      try {
-        const token = await this.getToken()
-        this.$auth.setUserToken(token)
+import { VueLoading } from 'vue-loading-template'
 
-        const user = await this.getUser()
-        this.$auth.setUser(user)
-        
-        this.$router.replace('/')
-      } catch (e) {
-        this.$toast.error(e.message)
-      }
+export default {
+  components: {
+    VueLoading
+  },
+  async mounted() {
+    try {
+      const token = await this.getToken()
+      this.$auth.setUserToken(token)
+
+      const user = await this.getUser()
+      this.$auth.setUser(user)
+      
+      this.$router.replace('/')
+    } catch (e) {
+      this.$toast.error(e.message)
+    }
+  },
+  methods: {
+    async getToken() {
+      const res = await this.$axios.$get('oauth/google/callback', { params: this.$route.query })
+      this.showMessage(res.message)
+      return res.token
     },
-    methods: {
-      async getToken() {
-        const res = await this.$axios.$get('oauth/google/callback', { params: this.$route.query })
-        this.showMessage(res.message)
-        return res.token
-      },
-      async getUser() {
-        const res = await this.$axios.$get('user')
-        return res.user
-      },
-      showMessage(message) {
-        this.$toast.show(message)
-      }
+    async getUser() {
+      const res = await this.$axios.$get('user')
+      return res.user
+    },
+    showMessage(message) {
+      this.$toast.show(message)
     }
   }
+}
 </script>
