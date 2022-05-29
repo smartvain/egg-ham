83aadc4d11951e6e1c421b75d99aae0ee8e7d1fb
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-05-29 18:39:12 +0900

    add: usersテーブルに格納可能なカラムを追加

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -21,6 +21,9 @@ class User extends Authenticatable implements MustVerifyEmail
         'name',
         'email',
         'password',
+        'avatar',
+        'provider_id',
+        'provider_name'
     ];
 
     /**
