302f5054b74496384f30168c6bd22ef1f7713f12
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-05-29 23:09:52 +0900

    add: twitter認証のページを追加

---
+++ front/pages/oauth/twitter/callback.vue
@@ -0,0 +1,54 @@
+<template>
+<v-container fill-height>
+  <v-row justify="center" align-content="center">
+    <v-col cols="12" align="center">
+      <v-card-text class="text-h5" style="color: #979797">
+        Twitter認証中…
+      </v-card-text>
+
+      <VueLoading
+        type="bars"
+        color="#1095FE"
+        :size="{ width: '50px', height: '50px' }"
+      />
+    </v-col>
+  </v-row>
+</v-container>
+</template>
+
+<script>
+import { VueLoading } from 'vue-loading-template'
+
+export default {
+  components: {
+    VueLoading
+  },
+  async mounted() {
+    try {
+      const token = await this.getToken()
+      this.$auth.setUserToken(token)
+
+      const user = await this.getUser()
+      this.$auth.setUser(user)
+      
+      this.$router.replace('/')
+    } catch (e) {
+      this.$toast.error(e.message)
+    }
+  },
+  methods: {
+    async getToken() {
+      const res = await this.$axios.$get('oauth/twitter/callback', { params: this.$route.query })
+      this.showMessage(res.message)
+      return res.token
+    },
+    async getUser() {
+      const res = await this.$axios.$get('user')
+      return res.user
+    },
+    showMessage(message) {
+      this.$toast.show(message)
+    }
+  }
+}
+</script>
