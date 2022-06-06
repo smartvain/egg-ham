4c81d942d8bb8255ff3bbe1c63ce74df36ebffcc
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-06-06 22:39:25 +0900

    refactor: インデントを修正

--- api/routes/api.php
+++ api/routes/api.php
@@ -19,30 +19,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('login', [LoginController::class, 'login']);
-Route::post('register', [RegisterController::class, 'register']);
-Route::get('email/verify/{id}', [VerificationController::class, 'verify'])->name('verification.verify');
-Route::get('email/resend', [VerificationController::class, 'resend'])->name('verification.resend');
+Route::post('login',            [LoginController::class,        'login'   ]);
+Route::post('register',         [RegisterController::class,     'register']);
+Route::get('email/verify/{id}', [VerificationController::class, 'verify'  ])->name('verification.verify');
+Route::get('email/resend',      [VerificationController::class, 'resend'  ])->name('verification.resend');
 
-Route::get('captions', [YoutubeController::class, 'getCaptions']);
-Route::get('langList', [YoutubeController::class, 'getLangList']);
+Route::get('captions',  [YoutubeController::class, 'getCaptions' ]);
+Route::get('langList',  [YoutubeController::class, 'getLangList' ]);
 Route::get('videoInfo', [YoutubeController::class, 'getVideoInfo']);
 
-Route::post('translate', [DeepLController::class, 'translate']);
+Route::post('translate',       [DeepLController::class, 'translate'        ]);
 Route::post('character_count', [DeepLController::class, 'getCharacterCount']);
 
 Route::group(['middleware' => ['session'], 'prefix' => 'oauth'], function () {
-    Route::get('{provider}/redirect', [LoginController::class, 'socialRedirect']);
-    Route::get('google/callback', [LoginController::class, 'handleGoogleCallback']);
-    Route::get('twitter/callback', [LoginController::class, 'handleTwitterCallback']);
+    Route::get('{provider}/redirect', [LoginController::class, 'socialRedirect'       ]);
+    Route::get('google/callback',     [LoginController::class, 'handleGoogleCallback' ]);
+    Route::get('twitter/callback',    [LoginController::class, 'handleTwitterCallback']);
 });
 
 Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
     Route::get('user', [LoginController::class, 'user']);
     
-    Route::get('words', [WordController::class, 'getWords']);
-    Route::put('words', [WordController::class, 'saveWords']);
-    Route::post('words', [WordController::class, 'storeWords']);
+    Route::get('words',            [WordController::class, 'getWords'  ]);
+    Route::put('words',            [WordController::class, 'saveWords' ]);
+    Route::post('words',           [WordController::class, 'storeWords']);
     Route::delete('word/{wordId}', [WordController::class, 'deleteWord']);
 });
 
