b7f5629ccd989f6dd6c862d8704082390e77d0a1
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-06-10 16:47:49 +0900

    add: DatabaseSeederにToeflWordSeederを追加

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            WordSeeder::class
+            WordSeeder::class,
+            ToeflWordSeeder::class
         ]);
     }
 }
