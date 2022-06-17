b2e5b9a338d434a4fda5730c36ee0ed1298f0fec
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-06-17 22:04:37 +0900

    add: パスワードを変更するバックエンド処理を追加

--- api/app/Http/Controllers/Auth/ChangePasswordController.php
+++ api/app/Http/Controllers/Auth/ChangePasswordController.php
@@ -3,9 +3,56 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class ChangePasswordController extends Controller
 {
-    //
+    private $user;
+    
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+    
+    public function changePassword(Request $request)
+    {
+        $currentPass = $request->currentPass;
+        $newPass     = $request->newPass;
+        $confirmPass = $request->confirmPass;
+        $email       = $request->email;
+        
+        $user = User::where('email', $email)->first();
+        
+        $defaultMessage = '正常に処理が行われませんでした。もう一度お試しください。';
+        $successMessage = 'パスワードの変更に成功しました。';
+        $errorMessage   = [
+            'email'       => 'ユーザーが存在しません。',
+            'currentPass' => '現在のパスワードが違います。',
+            'confirmPass' => '確認パスワードが違います。',
+        ];
+        
+        $message = $defaultMessage;
+        
+        if (!$user) {
+            $message = $errorMessage['email'];
+            $isSuccess = false;
+        } elseif (!Hash::check($currentPass, $user->password)) {
+            $message = $errorMessage['currentPass'];
+            $isSuccess = false;
+        } elseif ($newPass !== $confirmPass) {
+            $message = $errorMessage['confirmPass'];
+            $isSuccess = false;
+        } else {
+            $this->user->replace([
+                'id'       => $user->id,
+                'password' => Hash::make($newPass)
+            ]);
+            $message = $successMessage;
+            $isSuccess = true;
+        }
+
+        return ['message' => $message, 'isSuccess' => $isSuccess];
+    }
 }
