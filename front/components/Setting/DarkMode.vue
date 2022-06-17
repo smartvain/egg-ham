bf42c6db3cfc47e819b779a125e7b90a8c6face0
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-06-17 20:47:16 +0900

    add: ダークモードコンポーネントを実装

--- front/components/Setting/DarkMode.vue
+++ front/components/Setting/DarkMode.vue
@@ -1,12 +1,50 @@
 <template>
   <div>
     <v-card-title>ダークモード設定</v-card-title>
+
+    <v-card-text>
+      <v-switch
+        v-model="localTheme"
+        :prepend-icon="themeIcon"
+      />
+    </v-card-text>
   </div>
 </template>
 
 <script>
-export default {
+import { mapGetters } from 'vuex'
 
+export default {
+  data: () => ({
+    loading: {
+      changeTheme: false
+    },
+    localTheme: false
+  }),
+  computed: {
+    ...mapGetters(['theme']),
+    themeIcon() {
+      return this.localTheme ? 'mdi-weather-night' : 'mdi-weather-sunny'
+    }
+  },
+  created() {
+    this.localTheme = this.theme
+  },
+  watch: {
+    async localTheme(value) {
+      this.loading.changeTheme = true
+      
+      this.$vuetify.theme.dark = value
+      this.$store.commit('setTheme', value)
+      try {
+        await this.$axios.$put('user/theme', { theme: value, id: this.$auth.user.id })
+      } catch (e) {
+        this.$toast.error(e.message)
+      }
+
+      this.loading.changeTheme = false
+    }
+  },
 }
 </script>
 
