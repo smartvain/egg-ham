2561025ae7604a52e253070f248ea1d92010ff64
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-06-18 16:50:14 +0900

    fix: stateless()がありませんエラーを解消

--- api/app/Http/Controllers/Auth/LoginController.php
+++ api/app/Http/Controllers/Auth/LoginController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Laravel\Socialite\Facades\Socialite;
+use Socialite;
 
 class LoginController extends Controller
 {
