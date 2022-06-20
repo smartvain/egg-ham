aaa25801b6409f45dac3a54c5b538a5dd78c802f
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-06-20 20:46:41 +0900

    refactor: UserInfoコンポーネントをリファクター

--- front/components/Setting/UserInfo.vue
+++ front/components/Setting/UserInfo.vue
@@ -2,21 +2,19 @@
   <div>
     <v-card-title class="mb-3">アカウント情報</v-card-title>
 
-    <template>
+    <div>
       <v-card-text v-if="userInfo.name !== null">
         名前：
         <span v-if="editMode">
           <v-text-field
-            dense outlined
-            class="d-inline-block"
             v-model="userName"
+            class="d-inline-block"
+            dense outlined
             @input="isEdited = true"
           />
         </span>
         
-        <span v-else>
-          {{ userName }}
-        </span>
+        <span v-else>{{ userName }}</span>
 
         <v-btn
           color="primary"
@@ -32,25 +30,25 @@
       <v-card-text v-else>
         名前：<span class="grey--text lighten-4">なし</span>
       </v-card-text>
-    </template>
+    </div>
     
-    <v-card-text>
-      メールアドレス：{{ userInfo.email }}
-    </v-card-text>
+    <v-card-text>メールアドレス：{{ userInfo.email }}</v-card-text>
 
-    <template>
+    <div>
       <v-card-text v-if="userInfo.provider_name !== null">
         連携SNS：{{ userInfo.provider_name }}
       </v-card-text>
+
       <v-card-text v-else>
         連携SNS：<span class="grey--text lighten-4">なし</span>
       </v-card-text>
-    </template>
+    </div>
 
     <v-row>
       <v-col cols="2" class="pr-0">
         <v-card-text>アイコン：</v-card-text>
       </v-col>
+
       <v-col class="pl-0 mt-4">
         <v-img
           :src="$auth.user.avatar ? $auth.user.avatar : require('~/assets/img/default_user.png')"
