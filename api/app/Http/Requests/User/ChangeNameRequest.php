f218281fc31acf706defdd6889e073c12f0633a7
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-07-02 12:17:55 +0900

    change: バリデーション条件を変更

--- api/app/Http/Requests/User/ChangeNameRequest.php
+++ api/app/Http/Requests/User/ChangeNameRequest.php
@@ -24,7 +24,7 @@ class ChangeNameRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|string'
+            'name' => 'required|max:20'
         ];
     }
 }
