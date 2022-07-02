fe0209aaa0781e29ce054b7f3b2c656b0c2b6f29
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-07-02 11:59:52 +0900

    add: verificationcontrollerに値を追加

--- api/app/Http/Requests/VerificationRequest.php
+++ api/app/Http/Requests/VerificationRequest.php
@@ -13,7 +13,7 @@ class VerificationRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,10 @@ class VerificationRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'expires'   => 'required',
+            'hash'      => 'required',
+            'signature' => 'required',
+            'oldUserId' => 'numeric',
         ];
     }
 }
