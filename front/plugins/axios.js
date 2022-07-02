e64219e599e7bb8dd2153ee49e334771fe7a00d0
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-07-02 12:50:25 +0900

    change: エラー文言を変更

--- front/plugins/axios.js
+++ front/plugins/axios.js
@@ -6,7 +6,7 @@ export default ({ $axios, $toast}) => {
         $toast.error(`${code}: ユーザーが承認されていません。先にログインをお試し下さい。`)
         break
       case 422:
-        $toast.error(`${code}: 不正なアクセスを検知しました。`)
+        $toast.error(`${code}: 不正な入力値を検知しました。`)
         break
       case 500:
         $toast.error(`${code}: サーバーエラーが発生しました。時間をおいてもう一度お試しください。`)
