a22ba59d01008d4d42c0a2628e1bdbe73ef1238c
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-07-03 15:32:37 +0900

    refactor: ResetPasswordNotificationのuse宣言を整理

--- api/app/Notifications/ResetPasswordNotification.php
+++ api/app/Notifications/ResetPasswordNotification.php
@@ -4,9 +4,8 @@ namespace App\Notifications;
 
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
-use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Lang;
 
 class ResetPasswordNotification extends ResetPassword
 {
