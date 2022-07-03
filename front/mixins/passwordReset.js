f2deaf39f8592478b68f60f6e27b9f47f0340656
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-07-03 12:26:01 +0900

    add: 仮のパスワード再設定メール送信メソッドを定義

--- front/mixins/passwordReset.js
+++ front/mixins/passwordReset.js
@@ -1,7 +1,17 @@
 export default {
   methods: {
-    passwordReset() {
+    async passwordReset() {
+      this.loading.passwordReset = true
       
+      console.log(this.$auth.user)
+      try {
+        const res = await this.$axios.$post('password/request', { email: this.$auth.user.email })
+        this.showMessage(res.status, res.message)
+      } catch (e) {
+        console.log(e.message)
+      }
+
+      this.loading.passwordReset = false
     }
   }
 }
