9bf32e8ea91ecc92386908d696e874df020a907c
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-07-03 12:25:22 +0900

    refactor: 改行を調整

--- api/routes/api.php
+++ api/routes/api.php
@@ -22,8 +22,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('register',         RegisterController::class);
-Route::post('login',            [LoginController::class,        'login' ]);
+Route::post('register', RegisterController::class);
+
+Route::post('login', [LoginController::class, 'login']);
+
 Route::get('email/verify/{id}', [VerificationController::class, 'verify'])->name('verification.verify');
 Route::get('email/resend',      [VerificationController::class, 'resend'])->name('verification.resend');
 Route::post('password/reset',   PasswordResetController::class);
