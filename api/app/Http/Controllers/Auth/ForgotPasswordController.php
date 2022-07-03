90675c1c0d91dd94cf79751e1b74f817b12ad825
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-07-03 13:39:25 +0900

    change: ForgotPasswordControllerにファイル名変更

---
+++ api/app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\ResetPasswordRequest;
+use App\Http\Requests\Auth\SendResetLinkEmailRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Password;
+
+class ForgotPasswordController extends Controller
+{
+    public function sendResetLinkEmail(SendResetLinkEmailRequest $request)
+    {
+        $messages = $this->getSendResetLinkMessages();
+        $response = Password::sendResetLink($request->only('email'));
+        
+        if ($response !== Password::RESET_LINK_SENT) {
+            $status  = 'error';
+            $message = $messages[$status];
+        } else {
+            $status  = 'success';
+            $message = $messages[$status];
+        }
+
+        return compact('message', 'status');
+    }
+
+    public function resetPassword(ResetPasswordRequest $request)
+    {
+        $messages    = $this->getResetPasswordMessages();
+        $credentials = $request->only(['email', 'token', 'password']);
+
+        $response = Password::reset($credentials, function (User $user, string $password) {
+            $user->password = Hash::make($password);
+            $user->save();
+        });
+
+        if ($response !== Password::PASSWORD_RESET) {
+            $status  = 'error';
+            $message = $messages[$status];
+        } else {
+            $status  = 'success';
+            $message = $messages[$status];
+        }
+
+        $redirectUrl = config('app.home_url') . '/oauth/email/verify';
+        $param1      = "message={$message}";
+        $param2      = "&status={$status}";
+
+        return redirect("{$redirectUrl}?{$param1}{$param2}");
+    }
+
+    private function getSendResetLinkMessages()
+    {
+        return [
+            'success' => 'パスワード再設定メールを送信しました。',
+            'error'   => 'パスワード再設定メールの送信に失敗しました。'
+        ];
+    }
+
+    private function getResetPasswordMessages()
+    {
+        return [
+            'success' => 'パスワードをリセットしました。',
+            'error'   => 'パスワードリセットに失敗しました。'
+        ];
+    }
+}
