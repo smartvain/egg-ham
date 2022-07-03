47bfab3580b1c6914cfd78beb0e67bd36e632125
Ryuichi015210 <zlc.blue15210@gmail.com>
2022-07-03 13:41:50 +0900

    add: ResetPasswordNotificationを追加

---
+++ api/app/Notifications/ResetPasswordNotification.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ResetPasswordNotification extends ResetPassword
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $resetUrl = $this->resetUrl($notifiable);
+        
+        return (new MailMessage)
+            ->subject(Lang::get('mail.password_reset.subject'))
+            ->line(Lang::get('mail.password_reset.line_01'))
+            ->action(Lang::get('mail.password_reset.action'), $resetUrl)
+            ->line(Lang::get('mail.password_reset.line_02'));
+    }
+
+    public function resetUrl($notifiable)
+    {
+        if (static::$createUrlCallback) {
+            return call_user_func(static::$createUrlCallback, $notifiable, $this->token);
+        }
+
+        return url(route('password.reset', [
+            'token' => $this->token,
+            'email' => $notifiable->getEmailForPasswordReset(),
+        ], false));
+    }
+}
